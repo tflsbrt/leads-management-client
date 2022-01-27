4420fb2e3b25bcf564971f2b9b62434800052657
thais.felisberto <thais.felisberto@i-value.com.br>
2022-01-27 18:55:57 -0300

    Init client project

---
+++ src/App.js
@@ -0,0 +1,37 @@
+import { useState } from 'react';
+
+import Button from 'react-bootstrap/Button';
+
+import DialogModal from './components/DialogModal';
+
+import './styles/global.scss';
+
+const App = () => {
+  const [show, setShow] = useState(false);
+
+  const handleShow = () => setShow(true);
+
+  const handleConfirm = () => {
+    setShow(false);
+    alert('Yes, it is');
+  };
+
+  return (
+    <div className="main-page">
+      <h1>Microfrontend 2</h1>
+      <Button variant="warning" onClick={handleShow}>
+        Open Modal
+      </Button>
+
+      <DialogModal
+        title="Title"
+        message="This is happening inside microfrontend 2?"
+        isOpen={show}
+        onSubmit={() => handleConfirm()}
+        onClose={() => setShow(false)}
+      />
+    </div>
+  );
+};
+
+export default App;
---
+++ webpack.config.js
@@ -0,0 +1,81 @@
+const path = require('path');
+const deps = require('./package.json').dependencies;
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const ReactRefreshWebpackPlugin = require('@pmmmwh/react-refresh-webpack-plugin');
+const { ModuleFederationPlugin } = require('webpack').container;
+
+module.exports = () => {
+  const isDevelopment = process.env.NODE_ENV !== 'production';
+
+  return {
+    mode: isDevelopment ? 'development' : 'production',
+    devtool: isDevelopment ? 'eval-source-map' : 'source-map',
+    entry: path.resolve(__dirname, 'src', 'index.js'),
+    output: {
+      path: path.resolve('./build'),
+      filename: '[name].[contenthash].js',
+      publicPath: '/',
+      globalObject: 'this',
+      chunkLoadingGlobal: `webpackJsonp_sspahtmlwithjs`,
+    },
+    resolve: {
+      extensions: ['.jsx', '.js'],
+    },
+    devServer: {
+      static: path.resolve(__dirname, 'public'),
+      hot: true,
+      host: '0.0.0.0',
+      port: 5002,
+      headers: {
+        'Access-Control-Allow-Origin': '*',
+      },
+      allowedHosts: 'auto',
+    },
+    plugins: [
+      isDevelopment && new ReactRefreshWebpackPlugin(),
+      new HtmlWebpackPlugin({
+        template: path.resolve(__dirname, 'public', 'index.html'),
+      }),
+      new ModuleFederationPlugin({
+        name: 'client',
+        library: { type: 'var', name: 'client' },
+        filename: 'remoteEntry.js',
+        exposes: {
+          './App': './src/App',
+        },
+        shared: [
+          {
+            ...deps,
+            react: {
+              singleton: true,
+              requiredVersion: deps.react,
+            },
+            'react-dom': {
+              singleton: true,
+              requiredVersion: deps['react-dom'],
+            },
+          },
+        ],
+      }),
+    ].filter(Boolean),
+    module: {
+      rules: [
+        {
+          test: /\.(js|jsx)$/,
+          exclude: /node_modules/,
+          use: {
+            loader: 'babel-loader',
+            options: {
+              plugins: [isDevelopment && require.resolve('react-refresh/babel')].filter(Boolean),
+            },
+          },
+        },
+        {
+          test: /\.scss$/,
+          exclude: /node_modules/,
+          use: ['style-loader', 'css-loader', 'sass-loader'],
+        },
+      ],
+    },
+  };
+};
